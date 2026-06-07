df1add1cc8266ec7442eec8cf0c3158979eced68
29sujal <sujalyadav915@gmail.com>
2026-06-07 23:04:49 +0530

    added backened url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: '/api' })
+const API = axios.create({
+  baseURL:import.meta.env.VITE_API_URL || '/api'
+})
 
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user') || 'null')
@@ -10,11 +12,11 @@ API.interceptors.request.use((config) => {
 
 // Auth
 export const registerUser = (data) => API.post('/auth/register', data)
-export const loginUser    = (data) => API.post('/auth/login', data)
+export const loginUser = (data) => API.post('/auth/login', data)
 
 // Tasks
-export const getTasks    = (params) => API.get('/tasks', { params })
-export const createTask  = (data)   => API.post('/tasks', data)
-export const updateTask  = (id, data) => API.put(`/tasks/${id}`, data)
-export const toggleTask  = (id)     => API.patch(`/tasks/${id}/toggle`)
-export const deleteTask  = (id)     => API.delete(`/tasks/${id}`)
+export const getTasks = (params) => API.get('/tasks', { params })
+export const createTask = (data) => API.post('/tasks', data)
+export const updateTask = (id, data) => API.put(`/tasks/${id}`, data)
+export const toggleTask = (id) => API.patch(`/tasks/${id}/toggle`)
+export const deleteTask = (id) => API.delete(`/tasks/${id}`)
